a63033a03f54ae608086b8d8840689ce591525b6
milos018 <milos018@gmail.com>
2021-02-25 13:51:13 +0100

    finished navigation L48

--- react-app/src/shared/components/Navigation/MainNavigation.js
+++ react-app/src/shared/components/Navigation/MainNavigation.js
@@ -1,26 +1,47 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react'
+
 import { Link } from 'react-router-dom'
 
 import MainHeader from './MainHeader'
 import NavLinks from './NavLinks'
+import SideDrawer from './SideDrawer'
+import BackDrop from '../UIElements/Backdrop'
 
 import './MainNavigation.css'
 
 const MainNavigation = props => {
+
+  const [drawerisOpen, setDrawerIsOpen] = useState(false)
+  const openDrawerHandler = () => setDrawerIsOpen(true)
+  const closeDrawerHandler = () => setDrawerIsOpen(false)
+
   return (
-    <MainHeader>
-      <button className="main-navigation__menu-btn">
-        <span />
-        <span />
-        <span />
-      </button>
-      <h1 className="main-navigation__title">
-        <Link to="/">Your Places</Link>
-      </h1>
-      <nav>
-        <NavLinks />
-      </nav>
-    </MainHeader>
+    <Fragment>
+      {drawerisOpen && <BackDrop onClick={closeDrawerHandler} />}
+
+      <SideDrawer show={drawerisOpen} onClick={closeDrawerHandler}>
+        <nav className="main-navigation__drawer-nav">
+          <NavLinks />
+        </nav>
+      </SideDrawer>
+
+      <MainHeader>
+        <button
+          className="main-navigation__menu-btn"
+          onClick={openDrawerHandler}
+        >
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className="main-navigation__title">
+          <Link to="/">Your Places</Link>
+        </h1>
+        <nav className="main-navigation__header-nav">
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </Fragment>
   )
 }
 
