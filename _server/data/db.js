36c05247e6b62bd63b463cb93af3249c17ab4721
milos018 <milos018@gmail.com>
2021-04-24 22:26:28 +0200

    update react dependencies

--- _server/data/db.js
+++ _server/data/db.js
@@ -2,12 +2,13 @@ const { connect } = require('mongoose');
 
 module.exports = async () => {
 	try {
-		await connect(process.env.MONGO_URI, {
+		const conn = await connect(process.env.MONGO_URI, {
 			useNewUrlParser: true,
 			useFindAndModify: true,
 			useCreateIndex: true,
 			useUnifiedTopology: true,
 		});
+
 		console.log('Connected to DB');
 	} catch (error) {
 		console.log(error);
